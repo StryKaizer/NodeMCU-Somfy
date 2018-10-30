109447edc4bfcee017d79b300ea848068498ad14
Jimmy Henderickx <jimmyhdx@gmail.com>
2018-10-30 23:16:23 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,45 +1,29 @@
 config_file = "somfy."
 
-local tmr_cache = tmr.create()
-
-function deepcopy(orig)
-    local orig_type = type(orig)
-    local copy
-    if orig_type == 'table' then
-        copy = {}
-        for orig_key, orig_value in next, orig, nil do
-            copy[deepcopy(orig_key)] = deepcopy(orig_value)
-        end
-        setmetatable(copy, deepcopy(getmetatable(orig)))
-    else -- number, string, boolean, etc
-        copy = orig
-    end
-    return copy
-end
+local timer_write_delay = tmr.create()
 
 function readconfig()
-    local cfg, ok, ln
+    local config_string
     if file.exists(config_file .. "cfg") then
         print("Reading config from " .. config_file .. "cfg")
         file.open(config_file .. "cfg", "r+")
-        ln = file.readline()
+        config_string = file.readline()
         file.close()
     else
         if file.exists(config_file .. "bak") then
             print("Reading config from " .. config_file .. "bak")
             file.open(config_file .. "bak", "r+")
-            ln = file.readline()
+            config_string = file.readline()
             file.close()
         end
     end
-    if not ln then
-      ln = '{"demo":{"rc":1,"address":"123456"}}' 
-      print("When no devices are configured, the demo device will be shown.")
-      print("You need to add your own device(s) before you can remove the demo item.")
+    if not config_string or config_string == "{}\n" then
+        config_string = '{"demo":{"rc":1,"address":"123456"}}'
+        print("When no devices are configured, the demo device will be shown.")
+        print("You need to add your own device(s) before you can remove the demo item.")
     end
-    print("Configuration: " .. ln)
-    config = cjson.decode(ln)
-    config_saved = deepcopy(config)
+    print("Configuration: " .. config_string)
+    config = cjson.decode(config_string)
 end
 
 function writeconfighard()
@@ -47,44 +31,21 @@ function writeconfighard()
     file.remove(config_file .. "bak")
     file.rename(config_file .. "cfg", config_file .. "bak")
     file.open(config_file .. "cfg", "w+")
-    local ok, cfg = pcall(cjson.encode, config)
-    if ok then
-        file.writeline(cfg)
+    local encoding_succeeded, config_string = pcall(cjson.encode, config)
+    if encoding_succeeded then
+        file.writeline(config_string)
     else
         print("Saving to flash memory failed.")
     end
     file.close()
-
-    config_saved = deepcopy(config)
 end
 
+-- Writing file to flash memory causes the device to be unresponsive for a few seconds.
+-- Therefor, we delay these writes when the device is being used, until it is inactive again for 65 seconds.
 function writeconfig()
-    print("Requested save config...")
-    tmr.stop(tmr_cache)
-    local savenow = false
-    local savelater = false
-
-    --print("Config: "..cjson.encode(config))
-    --print("Config saved: "..cjson.encode(config))
-
-    local count = 0
-    for _ in pairs(config_saved) do count = count + 1 end
-    if count == 0 then
-        config_saved = readconfig()
-    end
-    for remote, cfg in pairs(config_saved) do
-        savelater = savelater or not config[remote] or config[remote].rc > cfg.rc
-        savenow = savenow or not config[remote] or config[remote].rc > cfg.rc + 10
-    end
-    savelater = savelater and not savenow
-    if savenow then
-        print("Saving config immediately!")
-        writeconfighard()
-    end
-    if savelater then
-        print("Delay triggered to save config")
-        tmr.alarm(tmr_cache, 65000, tmr.ALARM_SINGLE, writeconfighard)
-    end
+    tmr.stop(timer_write_delay)
+    print("Delay of 65 seconds triggered to save configuration to flash.")
+    tmr.alarm(timer_write_delay, 65000, tmr.ALARM_SINGLE, writeconfighard)
 end
 
 if not config then readconfig() end
